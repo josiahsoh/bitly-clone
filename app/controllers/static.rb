34e497ce8cd04b7d947efec72ba424071a112a18
Josiah <josiahsoh@gmail.com>
2015-11-26 00:34:47 -0800

    Revised User in Model

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,29 +1,3 @@
 get '/' do
-  @urls = Url.order(:id).all
-  @urls.each_with_index do |url,index|
-  	url.update_attributes(index_number: index+1)
-  end
   erb :"static/index"
-end
-
-post '/urls' do	
-	url = Url.create(long_url: params[:user_input_long_url])
-	redirect "/"
-end
-
-get '/:browser_short_url' do
-	url = Url.find_by(short_url: params[:browser_short_url])
-	url.click_count += 1
-	url.save
-	unless url.nil?
-		redirect url.long_url
-	else
-		redirect "/"
-	end
-end
-
-get '/delete/:id' do
-	id_num = params[:id]
-	url = Url.find_by(index_number: id_num).delete
-	redirect "/"
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,12 +1,29 @@
-get '/signup' do
-	erb :'users/new'
+get '/' do
+  @urls = Url.order(:id).all
+  @urls.each_with_index do |url,index|
+  	url.update_attributes(index_number: index+1)
+  end
+  erb :"static/index"
 end
 
-post '/signup' do
-	# Do something processing with user input
-	redirect to '/user/dashboard'
+post '/urls' do	
+	url = Url.create(long_url: params[:user_input_long_url])
+	redirect "/"
 end
 
-get '/user/dashboard' do
-	erb :dashboard
+get '/:browser_short_url' do
+	url = Url.find_by(short_url: params[:browser_short_url])
+	url.click_count += 1
+	url.save
+	unless url.nil?
+		redirect url.long_url
+	else
+		redirect "/"
+	end
+end
+
+get '/delete/:id' do
+	id_num = params[:id]
+	url = Url.find_by(index_number: id_num).delete
+	redirect "/"
 end
